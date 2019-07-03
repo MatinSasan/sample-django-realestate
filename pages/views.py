6e2738879b5b9d8782c0b3c012508ae4943c9749
Matin Sasanpour <matin.sasan@gmail.com>
2019-07-03 20:47:14 +0430

    added search page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -8,7 +9,10 @@ def index(request):
         '-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
 
     return render(request, 'pages/index.html', context)
